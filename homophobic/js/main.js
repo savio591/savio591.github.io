368732528f9798838f22dd444ce664df451bf56f
savio591 <savio591@hotmail.com>
2020-09-24 19:54:53 -0300

    Gay Detector v. 0.0.1

---
+++ homophobic/js/main.js
@@ -0,0 +1,29 @@
+let nome;
+let nomeReal
+let retorno;
+let nomesDeGay;
+let i = 0;
+
+nomesDeGay = "pedro" || "luan";
+
+function texto() {
+  nome = document.getElementById("entrada").value;
+  nomeReal = nome
+  retorno = nome.search(nomesDeGay);
+  nome = nome.replace("luan", "pedro");
+  retorno = nome.search(nomesDeGay);
+}
+
+function verificador() {
+  console.log("verificando...")
+  if ( i <= retorno) {
+    console.log( nomeReal+" "+"É GAY");
+    document.querySelector(".saida").innerHTML = nomeReal+" "+"É GAY!"
+  }
+  else {
+    console.log(nomeReal + " " + "não é gay.")
+    document.querySelector(".saida").innerHTML = nomeReal+" "+"não é gay"
+
+  }
+}
+
