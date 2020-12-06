a2cebd4218a696b5eb3efe32299e92684ce40053
savil experiments <savio591@hotmail.com>
2020-12-05 23:22:26 -0300

    social icons v2.0.3

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,25 +1,20 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+import social from '../assets/social_media.json'
+
+// import savilFetch from '../services/savilApi'
+
 import '../styles/footer.css';
-import discordIcon from "../assets/discord.svg";
-// import githubIcon from "../assets/github.svg";
-// import instagramIcon from "../assets/instagram.svg";
-// import linkedinIcon from "../assets/linkedin.svg";
+
 export default function Footer(props) {
     const copyrightText = props.copyrightText
 
-    const [socialData, setSocialData] = useState({})
 
-    useEffect(() => {
-        savilFetch.get('social').then(response => {
-            setProjects(response.data);
-        })
-    }, [])
-    
     return (
         <footer>
             <div className="socialIcons">
                 <ul>
-                    <li><a><img srcSet={}></img></a></li>
+                    <li><a href={social.instagram.url}><img srcSet={social.instagram.icon}></img></a></li>
                 </ul>
             </div>
             <h1>{copyrightText}
