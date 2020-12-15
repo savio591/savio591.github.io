06986d0dd5e2ebeda2acfd404ecf523097d7cb40
savil experiments <savio591@hotmail.com>
2020-12-14 21:47:51 -0300

    footer fix

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,24 +1,34 @@
 import React, { useState, useEffect } from "react";
 
-import social from '../assets/social_media.json'
-
-// import savilFetch from '../services/savilApi'
+import savilFetch from '../services/savilApi'
 
 import '../styles/footer.css';
 
 export default function Footer(props) {
-    const copyrightText = props.copyrightText
+    const copyrightText = props.copyrightText;
 
+    const [social, setSocial] = useState([])
+
+    useEffect(() => {
+        savilFetch.get('social_media').then(response => {
+            setSocial(response.data);
+        })
+    }, [])
 
     return (
+        <>
         <footer>
             <div className="socialIcons">
                 <ul>
-                    <li><a href={social.instagram.url}><img srcSet={social.instagram.icon}></img></a></li>
+                {social.map(social => (
+                    <li key={social.title}><a href={social.url}><img srcSet={social.icon}></img></a></li>
+                ))}
+                    
                 </ul>
             </div>
             <h1>{copyrightText}
             </h1>
         </footer>
+        </>
     )
 }
